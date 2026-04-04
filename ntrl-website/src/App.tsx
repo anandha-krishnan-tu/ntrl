830c6d375f71b6365a0b3f51a51bae58a57a11a0
anandha-krishnan-tu <anandhakrishnan.tu.official@gmail.com>
2026-04-05 00:08:39 +0530

    initial commit

---
+++ ntrl-website/src/App.tsx
@@ -0,0 +1,277 @@
+import './App.scss';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import MailIcon from '@mui/icons-material/Mail';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { useEffect, useState } from 'react';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import CheckIcon from '@mui/icons-material/Check';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+
+function App() {
+
+  const ScrollIntoSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  const [activeNav, setActiveNav] = useState("home");
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveNav(entry.target.id);
+          }
+        });
+      },
+      {
+        threshold: 0.6,
+      }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className="main-container">
+      <nav className="nav-container">
+        <div className="logo-container">
+          <img src="/ntrlLogoCropped.png" alt="" />
+        </div>
+
+        <ul className="navlinks">
+          <li className={activeNav === "home" ? "active" : ""} onClick={() => ScrollIntoSection("home")}>Home</li>
+          <li className={activeNav === "about" ? "active" : ""} onClick={() => ScrollIntoSection("about")}>About</li>
+          <li className={activeNav === "features" ? "active" : ""} onClick={() => ScrollIntoSection("features")}>Features</li>
+          <li className={activeNav === "plans" ? "active" : ""} onClick={() => ScrollIntoSection("plans")}>Plans</li>
+          <li className={activeNav === "success-stories" ? "active" : ""} onClick={() => ScrollIntoSection("success-stories")}>Success Stories</li>
+          <li className={activeNav === "contact-us" ? "active" : ""} onClick={() => ScrollIntoSection("contact-us")}>Contact Us</li>
+        </ul>
+
+        <button className="view-plans-btn">View Plans <ArrowCircleRightIcon /></button>
+      </nav>
+
+      <section className="home" id="home">
+        <div className="home-contents">
+          <div className="main-title">
+            NTRL
+          </div>
+          <div className="sub-title">
+            Nutrition - Training - Results - Longevity
+          </div>
+          <div className="cta-btn-container">
+            <button className="join-now-btn">Join Now <KeyboardDoubleArrowRightIcon className='icon' /></button>
+            <button className="contact-us-btn">Contact Us <MailIcon className='icon' /></button>
+          </div>
+          <div className="social-media-icons-container">
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon className='icon' />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon className='icon' />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+              <YouTubeIcon className='icon' />
+            </a>
+            <a href="https://t.me/" target="_blank" rel="noopener noreferrer">
+              <TelegramIcon className='icon' />
+            </a>
+            <a href="https://wa.me/" target="_blank" rel="noopener noreferrer">
+              <WhatsAppIcon className='icon' />
+            </a>
+            <a href="mailto:" target="_blank" rel="noopener noreferrer">
+              <MailIcon className='icon' />
+            </a>
+          </div>
+        </div>
+        <div className="home-backdrop"></div>
+      </section>
+
+      <section className="about" id="about">
+        <div className="about-container">
+          <div className="main-title">
+            Build a Strong, Lean Body — Without Guesswork
+          </div>
+          <div className="sub-title">
+            Personalized training and nutrition coaching designed to deliver real results and long-term transformation
+          </div>
+          <div className="paper-title">
+            Limited coaching slots available
+          </div>
+          <div className="button-container">
+            <button className="transformation-btn">Start My Transformation <KeyboardDoubleArrowRightIcon className='icon' /></button>
+            <button className="view-plans-btn">View Plans <ArrowCircleRightIcon className='icon' /></button>
+          </div>
+        </div>
+        <div className="about-backdrop"></div>
+      </section>
+
+      <section className="features" id="features">
+        <div className="features-contents">
+          <div className="main-title">
+            The NTRL System
+          </div>
+          <div className="sub-title">
+            No guesswork, No generic plans , just a system built for real results.
+          </div>
+          <div className="cards-container">
+            <div className="card">
+              <p className="title">Pernsolized Training Plans <TimelineIcon className='icon' /></p>
+              <p className="description">Structured workouts tailored to your goal, level, and schedule.</p>
+            </div>
+            <div className="card">
+              <p className="title">Practical Nutrition <HealthAndSafetyIcon className='icon' /></p>
+              <p className="description">Flexible meal plans in grams — simple, effective, and sustainable.</p>
+            </div>
+            <div className="card">
+              <p className="title">Real Accountability <SupportAgentIcon className='icon' /></p>
+              <p className="description">Regular check-ins and direct support to keep you consistent.</p>
+            </div>
+            <div className="card">
+              <p className="title">Progress Tracking <SignalCellularAltIcon className='icon' /></p>
+              <p className="description">Track results, make adjustments, and stay on track every week.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="features-backdrop"></div>
+      </section>
+
+      <section className="plans" id="plans">
+        <div className="plans-container">
+          <div className="main-title">
+            Start your transformation today
+          </div>
+          <div className="sub-title">
+            Choose a plan that fits your goal and start your fitness journey with structured coaching and real accountability.
+          </div>
+          <div className="cards-container">
+            <div className="card">
+              <div className="top-section">
+                <div className="head-tag">
+                  Kickstart Plan <CheckIcon className='icon'/>
+                </div>
+                <div className="week-text">
+                  4 Weeks Coaching
+                </div>
+                <div className="description-section">
+                  Recommended to continue for best results
+                </div>
+                <div className="price">
+                  AED 499
+                </div>
+              </div>
+              <div className="bottom-section">
+                <div className="description-section">
+                  Get started with structure, build habits and foundation
+                </div>
+                <ul className="plan-details">
+                  <li><CheckCircleIcon className='icon'/> Workout plan</li>
+                  <li><CheckCircleIcon className='icon'/> Basic nutrition guidance</li>
+                  <li><CheckCircleIcon className='icon'/> Weekly check-in</li>
+                  <li><CheckCircleIcon className='icon'/> Messaging support</li>
+                </ul>
+                <button className="start-plan-button">
+                  Start Kickstart Plan <ArrowRightIcon className='icon'/>
+                </button>
+              </div>
+            </div>
+            <div className="card popular">
+              <div className="top-section">
+                <div className="head-tag">
+                  <div className="left">
+                    Transformation Plan <StarBorderIcon className='icon'/>
+                  </div>
+                  <div className="right">
+                    | CORE PRODUCT
+                  </div>
+                </div>
+                <div className="week-text">
+                  8 Weeks Coaching
+                </div>
+                <div className="description-section">
+                  Minimum duration for visible transformation
+                </div>
+                <div className="price">
+                  AED 1,299
+                </div>
+              </div>
+              <div className="bottom-section">
+                <div className="description-section">
+                  This is your Real Program
+                </div>
+                <ul className="plan-details">
+                  <li><CheckCircleIcon className='icon'/> Full training program</li>
+                  <li><CheckCircleIcon className='icon'/> Detailed meal plan (grams/macros)</li>
+                  <li><CheckCircleIcon className='icon'/> Weekly adjustments</li>
+                  <li><CheckCircleIcon className='icon'/> Full support</li>
+                </ul>
+                <button className="start-plan-button">
+                  Start Transformation Plan <ArrowRightIcon className='icon'/>
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <div className="top-section">
+                <div className="head-tag">
+                  Elite Plan <AutoGraphIcon className='icon'/>
+                </div>
+                <div className="week-text">
+                  12 Weeks Coaching
+                </div>
+                <div className="description-section">
+                  Best results & long-term body change
+                </div>
+                <div className="price">
+                  AED 1,999
+                </div>
+              </div>
+              <div className="bottom-section">
+                <div className="description-section">
+                  This is where real transformation happens
+                </div>
+                <ul className="plan-details">
+                  <li><CheckCircleIcon className='icon'/> Everything included</li>
+                  <li><CheckCircleIcon className='icon'/> Deep tracking</li>
+                  <li><CheckCircleIcon className='icon'/> Lifestyle coaching</li>
+                  <li><CheckCircleIcon className='icon'/> Maximum accountability</li>
+                </ul>
+                <button className="start-plan-button">
+                  Start Elite Plan <ArrowRightIcon className='icon'/>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="success-stories" id="success-stories">
+
+      </section>
+
+      <section className="contact-us" id="contact-us">
+
+      </section>
+    </div>
+  )
+}
+
+export default App
